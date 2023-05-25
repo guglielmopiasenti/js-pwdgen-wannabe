f1c4d0d14f220ee987b6e4689fc925a398c1c25c
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-05-25 16:20:36 +0200

    asking questions on .js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('JS OK')
+console.log('JS OK');
+
+// Getting the element from the DOM
+
+const span = document.getElementById(pw);
+console.log(pw);
+
+// asking user's name and saving it
+
+const userFirstName = prompt('what is your first name?', 'Robert');
+console.log(userFirstName);
+
+// asking user's lastname and saving it
+
+const userLastName = prompt('what is your last name?', 'Jhonson');
+console.log(userLastName)
+
+// asking for favourite color and saving it 
+
+const userFavColor = prompt('when is your birthday?', );
+console.log(userFavColor);
+
+// Summing variables
+
+
